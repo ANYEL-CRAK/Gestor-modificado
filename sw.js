2f9d6466d436ad025950d48256c6493d2c4589c2
ANYEL-CRAK <135472120+ANYEL-CRAK@users.noreply.github.com>
2026-08-05 23:36:49 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'gestor-v1';
+const assets = ['./', './index.html', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
